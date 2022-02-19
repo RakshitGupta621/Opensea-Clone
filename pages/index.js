2b9225d9f1899aa289738b6c81b22d9857945aab
Rakshit Gupta <rakshitgupta621@gmail.com>
2022-02-19 22:43:42 +0530

    added hero

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-   <Header />
+  <>
+     <Header />
+     <Hero />
+  </>
   )
 }
